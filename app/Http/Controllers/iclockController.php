693f977db36fac648c6702d0d328ab7572bacf33
Hasan Basri <hasanbasri1493@gmail.com>
2024-11-30 15:55:15 +0700

    pint

--- app/Http/Controllers/iclockController.php
+++ app/Http/Controllers/iclockController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Attendance;
-use App\Models\Device;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
@@ -13,7 +11,6 @@ class iclockController extends Controller
 {
     public function __invoke(Request $request) {}
 
-    // handshake
     public function handshake(Request $request)
     {
         $data = [
@@ -24,13 +21,12 @@ class iclockController extends Controller
         ];
         DB::table('device_log')->insert($data);
 
-        // update status device
         DB::table('devices')->updateOrInsert(
             ['no_sn' => $request->input('SN')],
             ['online' => now()]
         );
 
-        $r = "GET OPTION FROM: {$request->input('SN')}\r\n".
+        return "GET OPTION FROM: {$request->input('SN')}\r\n".
             "Stamp=9999\r\n".
             'OpStamp='.time()."\r\n".
             "ErrorDelay=60\r\n".
@@ -44,30 +40,18 @@ class iclockController extends Controller
             //  "TimeZone=7\r\n" .
             "Realtime=1\r\n".
             'Encrypt=0';
-
-        return $r;
     }
 
-    //$r = "GET OPTION FROM:%s{$request->SN}\nStamp=".strtotime('now')."\nOpStamp=1565089939\nErrorDelay=30\nDelay=10\nTransTimes=00:00;14:05\nTransInterval=1\nTransFlag=1111000000\nTimeZone=7\nRealtime=1\nEncrypt=0\n";
-    // implementasi https://docs.nufaza.com/docs/devices/zkteco_attendance/push_protocol/
-    // setting timezone
-    // request absensi
     public function receiveRecords(Request $request)
     {
 
-        //DB::connection()->enableQueryLog();
         $content['url'] = json_encode($request->all());
         $content['data'] = $request->getContent();
         DB::table('finger_log')->insert($content);
         try {
-            // $post_content = $request->getContent();
-            //$arr = explode("\n", $post_content);
             $arr = preg_split('/\\r\\n|\\r|,|\\n/', $request->getContent());
-            //$tot = count($arr);
             $tot = 0;
-            //operation log
             if ($request->input('table') == 'OPERLOG') {
-                // $tot = count($arr) - 1;
                 foreach ($arr as $rey) {
                     if (isset($rey)) {
                         $tot++;
@@ -77,7 +61,6 @@ class iclockController extends Controller
 
                 return 'OK: '.$tot;
             }
-            //attendance
             foreach ($arr as $rey) {
                 if (empty($rey)) {
                     continue;
@@ -119,8 +102,6 @@ class iclockController extends Controller
 
     public function getrequest(Request $request)
     {
-        // $r = "GET OPTION FROM: ".$request->SN."\nStamp=".strtotime('now')."\nOpStamp=".strtotime('now')."\nErrorDelay=60\nDelay=30\nResLogDay=18250\nResLogDelCount=10000\nResLogCount=50000\nTransTimes=00:00;14:05\nTransInterval=1\nTransFlag=1111000000\nRealtime=1\nEncrypt=0";
-        // update status device
         DB::table('devices')->updateOrInsert(
             ['no_sn' => $request->input('SN')],
             ['online' => now()]
@@ -132,6 +113,5 @@ class iclockController extends Controller
     private function validateAndFormatInteger($value)
     {
         return isset($value) && $value !== '' ? (int) $value : null;
-        // return is_numeric($value) ? (int) $value : null;
     }
 }
