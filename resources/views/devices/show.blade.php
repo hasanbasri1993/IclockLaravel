1ee5bf7e46e28323d794b4ac2249b694e791bdf9
Hasan Basri <hasanbasri1493@gmail.com>
2024-11-30 14:35:47 +0700

    upgrade bootstrap and edit device

--- resources/views/devices/show.blade.php
+++ resources/views/devices/show.blade.php
@@ -6,10 +6,25 @@
         <form action="{{ route('devices.update', $device->id) }}" method="post" class="d-inline">
             @csrf
             @method('put')
-            <p>Nomor Serial: {{ $device->no_sn }}</p>
-            <p>Status: {{ $device->online }}</p>
-            <input type="text" class="form-text" name="nama" value="{{ $device->nama }}">
-            <input type="text" class="form-text"  name="lokasi" value="{{ $device->lokasi }}">
+            <p>Status: {{ \Carbon\Carbon::parse($device->online)->diffInMinutes(now()) < 2 ? 'ONLINE' : 'OFFLINE' }}</p>
+            <p>Last Online: {{ $device->online }}</p>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Nomor Serial</label>
+                <input type="text" class="form-control" disabled id="exampleFormControlInput1" placeholder=""
+                       name="no_sn"
+                       value="{{ $device->no_sn }}">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput2" class="form-label">Name </label>
+                <input type="text" class="form-control" id="exampleFormControlInput2" placeholder="" name="nama"
+                       value="{{ $device->nama }}">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput3" class="form-label">Lokasi</label>
+                <input type="text" class="form-control" id="exampleFormControlInput3" placeholder="" name="lokasi"
+                       value="{{ $device->lokasi }}">
+            </div>
+
             <button type="submit" class="btn btn-primary">Update</button>
         </form>
         <form action="{{ route('devices.destroy', $device->id) }}" method="post" class="d-inline">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ADMS Server</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.datatables.net/1.11.4/css/dataTables.bootstrap5.min.css" rel="stylesheet">
     <style>
         @media (max-width: 991.98px) {
@@ -73,7 +73,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
     <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.11.4/js/dataTables.bootstrap5.min.js"></script>
     <script>
         $(document).ready(function() {
